bef7bf35570c70cb76e3671665c5cf56ec761052
Kaio <Kaiofabiogit@gmail.com>
2026-02-17 16:19:25 -0300

    fix: ajuste no componente

--- frontend/src/views/Cadastro.vue
+++ frontend/src/views/Cadastro.vue
@@ -5,7 +5,6 @@
       <input v-model="nome" type="text" placeholder="Nome" required />
       <input v-model="email" type="email" placeholder="Email" required />
       <input v-model="senha" type="password" placeholder="Senha" required />
-      <input v-model="dadosBancarios" type="text" placeholder="Dados bancários" required />
 
       <button type="submit">Confirmar Cadastro</button>
       <router-link to="/">
@@ -25,7 +24,6 @@ import usuarioService from '../services/usuarioService'
 const nome = ref('')
 const email = ref('')
 const senha = ref('')
-const dadosBancarios = ref('')
 const erro = ref('')
 const sucesso = ref(false)
 
@@ -37,7 +35,6 @@ const handleCadastro = async () => {
       nome: nome.value,
       email: email.value,
       senha: senha.value,
-      dadosBancarios: dadosBancarios.value,
     })
     sucesso.value = true
   } catch (e: any) {
@@ -45,7 +42,6 @@ const handleCadastro = async () => {
   }
 }
 </script>
-
 
 <style scoped>
 .erro { color: red; }
