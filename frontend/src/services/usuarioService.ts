44d7818328509403a3da25fc071d18a4195aa77c
Kaio <Kaiofabiogit@gmail.com>
2026-02-18 15:11:12 -0300

    feat: ajustes usuarioService.ts

--- frontend/src/services/usuarioService.ts
+++ frontend/src/services/usuarioService.ts
@@ -8,9 +8,21 @@ const usuarioService = {
     },
 
     async login(email: string, senha: string): Promise<Usuario> {
-        const response = await api.post<Usuario>(`/usuarios/login?email=${email}&senha=${senha}`);
+        const response = await api.post<Usuario>(
+            `/usuarios/login?email=${email}&senha=${senha}`
+        );
         return response.data;
+    },
+
+    async logout(): Promise<void> {
+        await api.post("/usuarios/logout");
+    },
+
+    async getUsuario(): Promise<Usuario> {
+        const response = await api.get<Usuario>("/usuarios/me")
+        return response.data
     }
+
 };
 
 export default usuarioService;
