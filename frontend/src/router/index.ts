e524e829ed7c6793b0fff26a83d4720b05861f23
Kaio <Kaiofabiogit@gmail.com>
2026-02-18 15:10:30 -0300

    feat: ajustes index.ts

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,11 +2,15 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Welcome from '../views/Welcome.vue'
 import Cadastro from '../views/Cadastro.vue'
 import Login from '../views/Login.vue'
+import Filmes from '../views/Filmes.vue' // nova tela protegida
+import usuarioService from '../services/usuarioService'
+
 
 const routes = [
     { path: '/', name: 'Welcome', component: Welcome },
     { path: '/cadastro', name: 'Cadastro', component: Cadastro },
     { path: '/login', name: 'Login', component: Login },
+    { path: '/filmes', name: 'Filmes', component: Filmes, meta: { requiresAuth: true } }
 ]
 
 const router = createRouter({
@@ -14,4 +18,24 @@ const router = createRouter({
     routes,
 })
 
+router.beforeEach(async (to, from, next) => {
+    if (to.meta.requiresAuth) {
+        try {
+            await usuarioService.getUsuario() // chama /api/usuarios/me
+            next()
+        } catch (err: any) {
+            if (err.response?.status === 440) {
+                alert("Sessão expirada ou não iniciada. Faça login novamente.")
+                return next("/login")
+            }
+            return next("/login")
+        }
+    } else {
+        next()
+    }
+})
 export default router
+
+
+
+
